954a719296ff14f8e94d8d1162ab93d3b0ab2c14
Marcell Guilherme Costa da Silva <Mazuh@users.noreply.github.com>
2022-06-15 06:36:47 -0300

    chore: updates for fastify from 3.29.0 to 4.0.2 (#74)

--- examples/fastify-casbin-rest-example/index.js
+++ examples/fastify-casbin-rest-example/index.js
@@ -1,7 +1,7 @@
 const { PG_CONNECTION_STRING, CASBIN_ADAPTER, JWT_SECRET } = process.env
 
 module.exports = async function (fastify, opts) {
-  fastify
+  await fastify
     .register(require('fastify-jwt'), {
       secret: JWT_SECRET
     })
